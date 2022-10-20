c3d2783fe29117dd26e4c1048178140891bbc5a7
satechmedia <satechmedia@gmail.com>
2022-10-20 13:37:04 -0700

    more functions nested loops 10

--- 0x04-more_functions_nested_loops/10-print_triangle2.c
+++ 0x04-more_functions_nested_loops/10-print_triangle2.c
@@ -7,25 +7,26 @@
  */
 void print_triangle(int size)
 {
-	int sp, ro, tr;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+       	else
 	{
-		for (ro = 0; ro <= (size - 1); ro++)
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			for (sp = 0; sp < (size - 1) - ro; sp++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (tr = 0; tr <= ro; tr++)
-			{	
+
+			for (j = 1; j <= i; j++)
+			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
 	}
-}
+}	
